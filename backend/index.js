a32618b0afc3a0da7cdfe52527ab70386a98d00e
naqila syaniwa <nasyaaa312@gmail.com>
2025-10-26 15:46:13 +0700

    fix server part 1

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,20 @@
-import express from "express";
-import cors from "cors";
-import registerRoute from "./routes/register.js";
-import profileRoute from "./routes/profile.js";
-import loginRoutes from "./routes/login.js";
+// import express from "express";
+// import cors from "cors";
+// import registerRoute from "./routes/register.js";
+// import profileRoute from "./routes/profile.js";
+// import loginRoutes from "./routes/login.js";
 
-const app = express();
-const PORT = 5000;
+// const app = express();
+// const PORT = 5000;
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-// Route
-app.use("/api/register", registerRoute);
-app.use("/api/profile", profileRoute);
-app.use("/api/login", loginRoutes);
+// // Route
+// app.use("/api/register", registerRoute);
+// app.use("/api/profile", profileRoute);
+// app.use("/api/login", loginRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
